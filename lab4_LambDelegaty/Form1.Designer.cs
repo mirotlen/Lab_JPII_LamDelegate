e717b739f9f67a6a972c45adad72b5ff40d3b08f
Viktoria Girikovich <viktoria.girikovich@gmail.com>
2020-04-11 20:52:15 +0200

    first commit

---
+++ lab4_LambDelegaty/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace lab4_LambDelegaty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.but_Pow = new System.Windows.Forms.Button();
+            this.but_Con = new System.Windows.Forms.Button();
+            this.but_Rand = new System.Windows.Forms.Button();
+            this.but_Tab = new System.Windows.Forms.Button();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.but_SortMethod = new System.Windows.Forms.Button();
+            this.but_Sort_Asc_Desc = new System.Windows.Forms.Button();
+            this.but_Sort_Even_Odd = new System.Windows.Forms.Button();
+            this.but_9 = new System.Windows.Forms.Button();
+            this.but_Sort_Alfabet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(34, 111);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 102);
+            this.textBox3.TabIndex = 2;
+            // 
+            // but_Pow
+            // 
+            this.but_Pow.Location = new System.Drawing.Point(208, 20);
+            this.but_Pow.Name = "but_Pow";
+            this.but_Pow.Size = new System.Drawing.Size(131, 35);
+            this.but_Pow.TabIndex = 3;
+            this.but_Pow.Text = "Pow";
+            this.but_Pow.UseVisualStyleBackColor = true;
+            this.but_Pow.Click += new System.EventHandler(this.but_Pow_Click);
+            // 
+            // but_Con
+            // 
+            this.but_Con.Location = new System.Drawing.Point(208, 61);
+            this.but_Con.Name = "but_Con";
+            this.but_Con.Size = new System.Drawing.Size(131, 35);
+            this.but_Con.TabIndex = 4;
+            this.but_Con.Text = "Concat";
+            this.but_Con.UseVisualStyleBackColor = true;
+            this.but_Con.Click += new System.EventHandler(this.but_Con_Click);
+            // 
+            // but_Rand
+            // 
+            this.but_Rand.Location = new System.Drawing.Point(208, 103);
+            this.but_Rand.Name = "but_Rand";
+            this.but_Rand.Size = new System.Drawing.Size(131, 35);
+            this.but_Rand.TabIndex = 5;
+            this.but_Rand.Text = "Random";
+            this.but_Rand.UseVisualStyleBackColor = true;
+            this.but_Rand.Click += new System.EventHandler(this.but_Rand_Click);
+            // 
+            // but_Tab
+            // 
+            this.but_Tab.Location = new System.Drawing.Point(208, 144);
+            this.but_Tab.Name = "but_Tab";
+            this.but_Tab.Size = new System.Drawing.Size(131, 35);
+            this.but_Tab.TabIndex = 6;
+            this.but_Tab.Text = "Tab";
+            this.but_Tab.UseVisualStyleBackColor = true;
+            this.but_Tab.Click += new System.EventHandler(this.but_Tab_Click);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(31, 31);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(14, 13);
+            this.labelX.TabIndex = 7;
+            this.labelX.Text = "X";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(31, 72);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(14, 13);
+            this.labelY.TabIndex = 8;
+            this.labelY.Text = "Y";
+            // 
+            // but_SortMethod
+            // 
+            this.but_SortMethod.Location = new System.Drawing.Point(208, 185);
+            this.but_SortMethod.Name = "but_SortMethod";
+            this.but_SortMethod.Size = new System.Drawing.Size(131, 35);
+            this.but_SortMethod.TabIndex = 9;
+            this.but_SortMethod.Text = "SortMethod";
+            this.but_SortMethod.UseVisualStyleBackColor = true;
+            this.but_SortMethod.Click += new System.EventHandler(this.but_SortMethod_Click);
+            // 
+            // but_Sort_Asc_Desc
+            // 
+            this.but_Sort_Asc_Desc.Location = new System.Drawing.Point(345, 20);
+            this.but_Sort_Asc_Desc.Name = "but_Sort_Asc_Desc";
+            this.but_Sort_Asc_Desc.Size = new System.Drawing.Size(131, 35);
+            this.but_Sort_Asc_Desc.TabIndex = 10;
+            this.but_Sort_Asc_Desc.Text = "Sort Asc/Desc";
+            this.but_Sort_Asc_Desc.UseVisualStyleBackColor = true;
+            this.but_Sort_Asc_Desc.Click += new System.EventHandler(this.but_Sort_Asc_Desc_Click);
+            // 
+            // but_Sort_Even_Odd
+            // 
+            this.but_Sort_Even_Odd.Location = new System.Drawing.Point(345, 61);
+            this.but_Sort_Even_Odd.Name = "but_Sort_Even_Odd";
+            this.but_Sort_Even_Odd.Size = new System.Drawing.Size(131, 35);
+            this.but_Sort_Even_Odd.TabIndex = 11;
+            this.but_Sort_Even_Odd.Text = "Sort Even/Odd";
+            this.but_Sort_Even_Odd.UseVisualStyleBackColor = true;
+            this.but_Sort_Even_Odd.Click += new System.EventHandler(this.but_Sort_Even_Odd_Click);
+            // 
+            // but_9
+            // 
+            this.but_9.Location = new System.Drawing.Point(346, 144);
+            this.but_9.Name = "but_9";
+            this.but_9.Size = new System.Drawing.Size(130, 35);
+            this.but_9.TabIndex = 12;
+            this.but_9.Text = "9 punkt nie wiadomo po co zrobiony";
+            this.but_9.UseVisualStyleBackColor = true;
+            this.but_9.Click += new System.EventHandler(this.but_9_Click);
+            // 
+            // but_Sort_Alfabet
+            // 
+            this.but_Sort_Alfabet.Location = new System.Drawing.Point(346, 102);
+            this.but_Sort_Alfabet.Name = "but_Sort_Alfabet";
+            this.but_Sort_Alfabet.Size = new System.Drawing.Size(130, 36);
+            this.but_Sort_Alfabet.TabIndex = 13;
+            this.but_Sort_Alfabet.Text = "Sort Alfabet";
+            this.but_Sort_Alfabet.UseVisualStyleBackColor = true;
+            this.but_Sort_Alfabet.Click += new System.EventHandler(this.but_Sort_Alfabet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 249);
+            this.Controls.Add(this.but_Sort_Alfabet);
+            this.Controls.Add(this.but_9);
+            this.Controls.Add(this.but_Sort_Even_Odd);
+            this.Controls.Add(this.but_Sort_Asc_Desc);
+            this.Controls.Add(this.but_SortMethod);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.but_Tab);
+            this.Controls.Add(this.but_Rand);
+            this.Controls.Add(this.but_Con);
+            this.Controls.Add(this.but_Pow);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button but_Pow;
+        private System.Windows.Forms.Button but_Con;
+        private System.Windows.Forms.Button but_Rand;
+        private System.Windows.Forms.Button but_Tab;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Button but_SortMethod;
+        private System.Windows.Forms.Button but_Sort_Asc_Desc;
+        private System.Windows.Forms.Button but_Sort_Even_Odd;
+        private System.Windows.Forms.Button but_9;
+        private System.Windows.Forms.Button but_Sort_Alfabet;
+    }
+}
+
---
+++ lab4_LambDelegaty/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4_LambDelegaty
+{
+    public partial class Form1 : Form
+    {
+
+        List<Student> students = new List<Student>
+            {
+                new Student(1),
+                new Student(4),
+                new Student(8),
+                new Student(9),
+                new Student(67),
+                new Student(43),
+                new Student(32),
+                new Student(39),
+                new Student(82),
+                new Student(30),
+                new Student(32),
+                new Student(36)
+            };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         public static Random random = new Random();
+
+        private void but_Pow_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text, out int x);
+            int.TryParse(textBox2.Text, out int y);
+            Func<double> result = () => Math.Pow(x, y);
+            textBox3.Text = result().ToString();
+        }
+
+        private void but_Con_Click(object sender, EventArgs e)
+        {
+            string x = textBox1.Text.ToString();
+            string y = textBox2.Text.ToString();
+            Func<string> cocncat = () => x + ' ' + y;
+            textBox3.Text = cocncat().ToString();
+        }
+
+        private void but_Rand_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text, out int x);
+            string chars = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-_+/~`";
+            string result = new string(
+                Enumerable.Repeat(chars, x)
+                      .Select(s => s[random.Next(s.Length)])
+                      .ToArray());
+            textBox3.Text = result;
+        }
+
+        private void but_Tab_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text, out int x);
+            int.TryParse(textBox2.Text, out int y);
+            int[] Arr = new int[] {2, 4, 8, 10, 3, 5, 7, 0, 1, 100};
+            y = Arr[y];
+            Func<double> result = () => Math.Pow(x, y);
+            textBox3.Text = result().ToString();
+        }
+
+        public delegate int myDelegate<T>(T first, T second);
+        //public static int mySortingMethod<T>(T firstDigit, T secondDigit) where T : IComparable<T>
+        public static int mySortingMethod_1(int firstDigit, int secondDigit)
+        {
+            return firstDigit.CompareTo(secondDigit);
+        }
+
+        private void but_SortMethod_Click(object sender, EventArgs e)
+        {
+            List<int> _l = new List<int>
+            {
+                10, 9, 1231, 1327, 6, 123
+            };
+
+            myDelegate<int> handler = mySortingMethod_1;
+            _l.Sort((x, y) => handler(x, y));
+            string str = "";
+            foreach (int i in _l)
+            {
+                str += i + " ";
+            }
+
+            textBox3.Text = str;
+        }
+
+        private void but_Sort_Asc_Desc_Click(object sender, EventArgs e)
+        {
+
+            List<Student> students = this.students;
+
+            students = students.OrderBy(student => student.studentId).ToList();
+
+            string studentsOrderedBy = "";
+            foreach (Student student in students)
+            {
+                studentsOrderedBy += student.studentId + " ";
+            }
+            studentsOrderedBy += "\n";
+            students = students.OrderByDescending(student => student.studentId).ToList();
+            foreach (Student student in students)
+            {
+                studentsOrderedBy += student.studentId + " ";
+            }
+
+            textBox3.Text = studentsOrderedBy;
+        }
+
+        private void but_Sort_Even_Odd_Click(object sender, EventArgs e)
+        {
+            List<Student> students = this.students;
+
+            Func<List<Student>, List<Student>> EvenOddLambda = (studentList) =>
+            {
+                List<Student> studentsEven = new List<Student>();
+                List<Student> studentsOdd = new List<Student>();
+                foreach (Student student in studentList)
+                {
+                    if (student.studentId % 2 == 0)
+                    {
+                        studentsEven.Add(student);
+                    }
+                    else
+                    {
+                        studentsOdd.Add(student);
+                    }
+                }
+
+                studentsEven = studentsEven.OrderBy(item => item.studentId).ToList();
+                //studentsOdd = studentsOdd.OrderBy(item => item.studentId).ToList();
+
+                studentsEven.AddRange(studentsOdd.OrderBy(item => item.studentId).ToList());
+
+                return studentsEven;
+            };
+
+            string sortedStudents = "";
+            foreach (Student student in EvenOddLambda(students))
+            {
+                sortedStudents += student.studentId + " ";
+            }
+
+            textBox3.Text = sortedStudents;
+        }
+
+        private void but_9_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text, out int x);
+            int.TryParse(textBox2.Text, out int y);
+
+            textBox3.Text = nehochu(x).Invoke(y).ToString(); 
+        }
+        
+        private Func<int, double> nehochu (int x)
+        {
+            Func<int, double> PowLambda = (digit) =>
+            {
+                return Math.Pow(digit, x);
+            };
+
+            return PowLambda;
+        }
+
+        private void but_Sort_Alfabet_Click(object sender, EventArgs e)
+        {
+            Func<string, List<string>> parseStringDigitsToInt = (inputNumbers) =>
+             {
+                 //List<int> result = new List<int>();
+                 List<string> intsAsString = inputNumbers.Split(',').ToList();
+                 //foreach (string inp in intsAsString)
+                 //{
+                 //    int.TryParse(inp, out int buf);
+                 //    result.Add(buf);
+                 //}
+                 //return result;
+                 return intsAsString;
+             };
+            
+            //Func<int, int> splittingBy = (number) =>
+            //{
+            //    int restNum = number;
+            //    List<int> result = new List<int>();
+            //    do
+            //    {
+            //        result.Add(restNum % 10);
+            //        restNum /= 10;
+            //    } while (restNum >= 1);
+            //    result.Reverse();
+
+            //    return result[0];
+            //};
+            //Func<int, int> getCount = (number) =>
+            //{
+            //    int restNum = number;
+            //    int i = 0;
+            //    do
+            //    {
+            //        restNum /= 10;
+            //        i++;
+            //    } while (restNum >= 1);
+
+            //    return i;
+            //};
+            //List<int> inputDigits = parseStringDigitsToInt.Invoke(textBox1.Text);
+            List<string> inputDigits = parseStringDigitsToInt.Invoke(textBox1.Text);
+            //inputDigits = inputDigits.OrderBy(value => splittingBy(value)).ToList();
+            inputDigits.Sort();
+            string resultStr = "";
+            //foreach (int str in parseStringDigitsToInt.Invoke(textBox1.Text))
+            foreach (string str in inputDigits)
+            {
+                resultStr += str + " ";
+            }
+            textBox3.Text = resultStr;
+        }
+    }
+}
